2930a8506d60e34b2d468b089b5bcfedf70a53f2
khani3s <felipenavas@gmail.com>
2011-01-14 01:53:17 -0200

    New code for benchmark

--- src/pingu.rb
+++ src/pingu.rb
@@ -3,7 +3,7 @@ require "benchmark"
 
 class Main
   
-  attr_reader :total_files, :total_folders, :total_size
+  attr_reader :total_files, :total_folders, :total_size, :ignored_count
   
   def initialize()
     @file_size = 0
@@ -11,7 +11,9 @@ class Main
     @total_files = 0
     @total_folders = 0
     @file_array = []
+    @ignored_count = 0
   end
+
   def scan(path)
     Dir.chdir(path)
     
@@ -19,16 +21,22 @@ class Main
   
     Find.find(Dir.pwd) do |file_path|
 
-      if File.ftype(file_path) == "file"
+      if File.file?(file_path)
         file_size = File.size(file_path)
         @total_size +=  file_size
         @total_files += 1
         @file_array << [file_path, file_size]
-      else
+      elsif File.directory?(file_path)
         @total_folders +=1
+      else
+        @ignored_count += 1
+        puts file_path + " ignorado!"
       end
+
     end
+
   end
+
 end
 
 #
@@ -74,11 +82,32 @@ Benchmark.bmbm do |x|
   x.report("### khani3s\n") {
     m = Main.new
     m.scan(ARGV[0])
-    puts "#{m.total_files} arquivos em #{m.total_folders} pastas com o tamanho total de #{m.total_size} bytes"
+    puts "#{m.total_size.to_f/(1024*1024)} MBytes em #{m.total_folders} diretorios e #{m.total_files} arquivos. #{m.ignored_count} ignorados."
   }
   x.report("### rebellis\n")  {
   p = Pingu.new
   p.scan Hash[:filesystem => ARGV[0]]
+  }
+  x.report("###khani3s magic\n"){
+    dir_size = 0
+    dir_count = 0
+    file_count = 0
+    ignored_count = 0
+
+    file_array  = []
+    Find.find(ARGV[0]) do |f|
+      if File.file?(f)
+        file_size = File.stat(f).size
+        dir_size += file_size 
+        file_count += 1
+        file_array << [f, file_size]
+      elsif File.directory?(f)
+        dir_count += 1
+      else
+        ignored_count += 1 
+      end
+    end
+    puts "#{dir_size.to_f/(1024*1024)} MBytes em #{dir_count} diretorios e #{file_count} arquivos. #{ignored_count} ignorados."
   }
 end
 
